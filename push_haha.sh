8d0f4c55766b8fac55c2eeb61aed3cfa6f9af3ed
zcy <zhangchongyang@rocedar.com>
2016-12-01 16:41:09 +0800

    add file 2016-12-01 16:41:09

---
+++ push_haha.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+########################	
+# 文件名：push_haha.sh
+# 文件全路径：/home/zcy/dev/git/haha/push_haha.sh
+# 作者：zcy
+# 创建时间：2016-12-01 16:38:05
+########################
+
+echo "----------------------------------------"
+echo "上传haha工程开始  `date '+%Y-%m-%d %H:%M:%S'`"
+cd /home/zcy/dev/git/haha
+git pull
+git add .
+git commit -m "add file `date '+%Y-%m-%d %H:%M:%S'`"
+git push
+echo "上传haha工程结束 `date '+%Y-%m-%d %H:%M:%S'`"
+echo "----------------------------------------"
